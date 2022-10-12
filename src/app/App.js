6842c889c5aa626e43852dba06fb57f9e37f6bd7
Gabi Parrello <gabio@Gabio-MacBookPro.local>
2022-10-12 12:33:02 -0600

    context configs ready

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,13 @@
+import AppContextProvider from "./contexts/AppContext/AppContextProvider";
 import "./global-styles.css";
 import RegisterPage from "./pages/Register";
 
 function App() {
-  return <RegisterPage />;
+  return (
+    <AppContextProvider>
+      <RegisterPage />
+    </AppContextProvider>
+  );
 }
 
 export default App;
