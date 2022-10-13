82fd540e6adab28506cf6c03c5a67150e4d11b8f
Gabi Parrello <gabio@Gabio-MacBookPro.local>
2022-10-13 12:44:16 -0600

    firebase configured

--- src/app/pages/Register/components/Layout/index.js
+++ src/app/pages/Register/components/Layout/index.js
@@ -1,5 +1,4 @@
 import { useContext, useState } from "react";
-import { registerUser } from "../../../../../api/users";
 import Card from "../../../../components/Card";
 import LoadingOverlay from "../../../../components/loaders/LoadingOverlay";
 import Logo from "../../../../components/Logo";
@@ -12,34 +11,7 @@ const RegisterLayout = () => {
   const [submitLoading, setSubmitLoading] = useState(false);
   const [submitError, setSubmitError] = useState(null);
 
-  const handleSubmit = async (formData) => {
-    const { name, photo } = formData;
-
-    if (!name || !photo) {
-      setSubmitError("Complete los datos");
-      return;
-    }
-
-    setSubmitLoading(true);
-
-    try {
-      const user = await registerUser(name, photo);
-      setAppData({
-        ...appData,
-        auth: {
-          user_id: user.id,
-        },
-        user: {
-          name,
-          photo,
-        },
-      });
-    } catch (e) {
-      setSubmitError(e.message);
-    }
-
-    setSubmitLoading(false);
-  };
+  const handleSubmit = async (formData) => {};
 
   return (
     <div className="register-layout-container">
--- src/app/router/AppRouter.js
+++ src/app/router/AppRouter.js
@@ -1,11 +1,13 @@
-import { useContext } from "react";
-import AppContext from "../contexts/AppContext";
 import HomePage from "../pages/Home";
 import RegisterPage from "../pages/Register";
 
 const AppRouter = () => {
-  const { data } = useContext(AppContext);
-  return <>{data?.auth?.user_id ? <HomePage /> : <RegisterPage />}</>;
+  return (
+    <>
+      {/* <HomePage /> */}
+      <RegisterPage />
+    </>
+  );
 };
 
 export default AppRouter;
