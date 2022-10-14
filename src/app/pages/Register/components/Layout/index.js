bacd596f2c8e65ff993ffa8951e40999c1f28f8b
Gabi Parrello <gabio@Gabio-MacBookPro.local>
2022-10-13 19:33:08 -0600

    register improvements, abstraction to hook

--- src/app/pages/Register/components/Layout/index.js
+++ src/app/pages/Register/components/Layout/index.js
@@ -1,55 +1,23 @@
-import { useContext, useState } from "react";
-import { registerUser } from "../../../../../api/users";
 import Card from "../../../../components/Card";
 import LoadingOverlay from "../../../../components/loaders/LoadingOverlay";
 import Logo from "../../../../components/Logo";
-import AppContext from "../../../../contexts/AppContext";
+import useRegisterUser from "../../hooks/useRegisterUser";
 import RegisterForm from "../RegisterForm";
+import useMediaQuery from "../../../../hooks/useMediaQuery";
 import "./styles.css";
 
 const RegisterLayout = () => {
-  const { data: appData, setData: setAppData } = useContext(AppContext);
-  const [submitLoading, setSubmitLoading] = useState(false);
-  const [submitError, setSubmitError] = useState(null);
-
-  const handleSubmit = async (formData) => {
-    const { name, photo } = formData;
-
-    if (!name || !photo) {
-      setSubmitError("Complete los datos");
-      return;
-    }
-
-    setSubmitLoading(true);
-
-    try {
-      const user = await registerUser(name, photo);
-      setAppData({
-        ...appData,
-        auth: {
-          user_id: user.id,
-        },
-        user: {
-          name,
-          photo,
-        },
-      });
-    } catch (e) {
-      setSubmitError(e.message);
-    }
-
-    setSubmitLoading(false);
-  };
-
+  const [loading, error, registerUser] = useRegisterUser();
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
     <div className="register-layout-container">
-      <Logo width={200} />
+      <Logo width={isMobile ? 120 : 200} />
       <Card className="card">
-        <LoadingOverlay visible={submitLoading} />
+        <LoadingOverlay visible={loading} />
         <span className="title">Bienvenidxs!</span>
         <span className="subtitle">Regístrate para poder twittear</span>
-        {submitError && <span className="error-message">{submitError}</span>}
-        <RegisterForm onSubmit={handleSubmit} />
+        {error && <span className="error-message">{error}</span>}
+        <RegisterForm onSubmit={registerUser} />
       </Card>
     </div>
   );
